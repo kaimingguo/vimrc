2edd20911aa1ffe05ff3685a31d006c71f14b6a2
Kai-Ming Guo <augustin.guo@gmail.com>
2019-11-18 23:41:25 +0800

    fixed pandoc openFile failed

--- scripts/wiki2html.sh
+++ scripts/wiki2html.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env csh -f
+#!/bin/csh -f
 
 set syntax = "$2"
 set ext = "$3"
@@ -18,11 +18,12 @@ set fname = `basename "$input" ."$ext"`
 set tpl = "$tpl_path/$tpl_default$tpl_ext"
 set output = "$outdir$fname.html"
 
-if ( { grep -oq '$$.*$$' $5 >/dev/null } ) then
+if ( { grep -oq '$$.*$$' "$input" >/dev/null } ) then
 	set math = '--mathjax=https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
 else
-	set math = ''
+	set math = '--mathjax'
 endif
 
-sed -E 's/[^\(!\)](\[.+\])\(([^#)]+)\)/\1(\2.html)/g' < "$input" | \
+sed -E 's/[^\(!\)](\[.+\])\(([^#)]+)\)/\1(\2.html)/g' <"$input" | \
 	pandoc "$math" --template="$tpl" -f "$syntax" -t html -c "$styles:t" -M root_path:"$root_path" > $output
+
